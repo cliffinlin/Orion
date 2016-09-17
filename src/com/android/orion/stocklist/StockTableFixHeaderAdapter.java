7fe2102900026a894bf016418f19dd196fa314fe
cliffinlin <cliffinlin@163.com>
2016-09-17 09:33:46 +0800

    update

--- src/com/android/orion/stocklist/StockTableFixHeaderAdapter.java
+++ src/com/android/orion/stocklist/StockTableFixHeaderAdapter.java
@@ -11,65 +11,66 @@ import com.miguelbcr.tablefixheaders.TableFixHeaderAdapter;
 public class StockTableFixHeaderAdapter
 		extends
 		TableFixHeaderAdapter<String, StockCellViewGroup, String, StockCellViewGroup, Nexus, StockCellViewGroup, StockCellViewGroup, StockCellViewGroup> {
-	private Context context;
+	Context mContext;
 
 	public StockTableFixHeaderAdapter(Context context) {
 		super(context);
-		this.context = context;
+		
+		mContext = context;
 	}
 
 	@Override
 	protected StockCellViewGroup inflateFirstHeader() {
-		return new StockCellViewGroup(context);
+		return new StockCellViewGroup(mContext);
 	}
 
 	@Override
 	protected StockCellViewGroup inflateHeader() {
-		return new StockCellViewGroup(context);
+		return new StockCellViewGroup(mContext);
 	}
 
 	@Override
 	protected StockCellViewGroup inflateFirstBody() {
-		return new StockCellViewGroup(context);
+		return new StockCellViewGroup(mContext);
 	}
 
 	@Override
 	protected StockCellViewGroup inflateBody() {
-		return new StockCellViewGroup(context);
+		return new StockCellViewGroup(mContext);
 	}
 
 	@Override
 	protected StockCellViewGroup inflateSection() {
-		return new StockCellViewGroup(context);
+		return new StockCellViewGroup(mContext);
 	}
 
 	@Override
 	protected List<Integer> getHeaderWidths() {
 		Integer[] witdhs = {
-				(int) context.getResources().getDimension(R.dimen._150dp),
-				(int) context.getResources().getDimension(R.dimen._120dp),
-				(int) context.getResources().getDimension(R.dimen._170dp),
-				(int) context.getResources().getDimension(R.dimen._80dp),
-				(int) context.getResources().getDimension(R.dimen._110dp),
-				(int) context.getResources().getDimension(R.dimen._80dp),
-				(int) context.getResources().getDimension(R.dimen._80dp) };
+				(int) mContext.getResources().getDimension(R.dimen._150dp),
+				(int) mContext.getResources().getDimension(R.dimen._120dp),
+				(int) mContext.getResources().getDimension(R.dimen._170dp),
+				(int) mContext.getResources().getDimension(R.dimen._80dp),
+				(int) mContext.getResources().getDimension(R.dimen._110dp),
+				(int) mContext.getResources().getDimension(R.dimen._80dp),
+				(int) mContext.getResources().getDimension(R.dimen._80dp) };
 
 		return Arrays.asList(witdhs);
 	}
 
 	@Override
 	protected int getHeaderHeight() {
-		return (int) context.getResources().getDimension(R.dimen._35dp);
+		return (int) mContext.getResources().getDimension(R.dimen._35dp);
 	}
 
 	@Override
 	protected int getSectionHeight() {
-		return (int) context.getResources().getDimension(R.dimen._25dp);
+		return (int) mContext.getResources().getDimension(R.dimen._25dp);
 	}
 
 	@Override
 	protected int getBodyHeight() {
-		return (int) context.getResources().getDimension(R.dimen._45dp);
+		return (int) mContext.getResources().getDimension(R.dimen._45dp);
 	}
 
 	@Override
