12a3df7d65988403398c8a02756ffc5d366047cd
cliffinlin <cliffinlin@github.com>
2022-10-05 18:43:38 +0800

    update

--- app/src/main/java/com/android/orion/MarketKeyAnalyzer.java
+++ app/src/main/java/com/android/orion/MarketKeyAnalyzer.java
@@ -63,84 +63,84 @@ public class MarketKeyAnalyzer {
             prev = dataList.get(i - 1);
             current = dataList.get(i);
 
-            Log.d(TAG, "i=" + i + " current.getDate()=" + current.getDate() + " current.getLow()=" + current.getLow() + " current.getHigh()=" + current.getHigh());
+//            Log.d(TAG, "i=" + i + " current.getDate()=" + current.getDate() + " current.getLow()=" + current.getLow() + " current.getHigh()=" + current.getHigh());
 
             switch (mMarketKeyType) {
                 case StockData.MARKET_KEY_SECONDARY_RALLY:
                     break;
 
                 case StockData.MARKET_KEY_NATURAL_RALLY:
-                    Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mNaturalRally=" + mNaturalRally);
+//                    Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mNaturalRally=" + mNaturalRally);
                     if (current.getHigh() > mNaturalRally) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.getHigh()=" + current.getHigh() + " > mNaturalRally=" +  mNaturalRally);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.getHigh()=" + current.getHigh() + " > mNaturalRally=" +  mNaturalRally);
                         setNaturalRally(current);
                         if (current.getHigh() > mPrevHigh * (1.0 + NATURAL_THRESHOLD / 2.0)) {
-                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.getHigh()=" + current.getHigh() + " > mPrevHigh * (1.0 + NATURAL_THRESHOLD / 2.0)=" + mPrevHigh * (1.0 + NATURAL_THRESHOLD / 2.0));
+//                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.getHigh()=" + current.getHigh() + " > mPrevHigh * (1.0 + NATURAL_THRESHOLD / 2.0)=" + mPrevHigh * (1.0 + NATURAL_THRESHOLD / 2.0));
                             mMarketKeyType = StockData.MARKET_KEY_UPWARD_TREND;
-                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mMarketKeyType = StockData.MARKET_KEY_UPWARD_TREND");
+//                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mMarketKeyType = StockData.MARKET_KEY_UPWARD_TREND");
                             setUpwardTrend(current);
                             current.setNaturalRally(0);
-                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.setNaturalRally(0)");
+//                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.setNaturalRally(0)");
                         }
                     } else if (current.getLow() < mNaturalRally * (1.0 - NATURAL_THRESHOLD)) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.getLow()=" + current.getLow() + " < mNaturalRally * (1.0 - NATURAL_THRESHOLD)=" + mNaturalRally * (1.0 - NATURAL_THRESHOLD));
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: current.getLow()=" + current.getLow() + " < mNaturalRally * (1.0 - NATURAL_THRESHOLD)=" + mNaturalRally * (1.0 - NATURAL_THRESHOLD));
                         mPrevHigh = mNaturalRally;
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mPrevHigh = mNaturalRally=" + mPrevHigh);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mPrevHigh = mNaturalRally=" + mPrevHigh);
                         mMarketKeyType = StockData.MARKET_KEY_NATURAL_REACTION;
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mMarketKeyType = StockData.MARKET_KEY_NATURAL_REACTION");
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_RALLY: mMarketKeyType = StockData.MARKET_KEY_NATURAL_REACTION");
                         setNaturalReaction(current);
                     }
                     break;
 
                 case StockData.MARKET_KEY_UPWARD_TREND:
-                    Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: mUpwardTrend=" + mUpwardTrend);
+//                    Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: mUpwardTrend=" + mUpwardTrend);
                     if (current.getHigh() > mUpwardTrend) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: current.getHigh()=" + current.getHigh() + " > mUpwardTrend=" + mUpwardTrend);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: current.getHigh()=" + current.getHigh() + " > mUpwardTrend=" + mUpwardTrend);
                         setUpwardTrend(current);
                     } else if (current.getLow() < mUpwardTrend * (1.0 - NATURAL_THRESHOLD)) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: current.getLow()=" + current.getLow() + " < mUpwardTrend * (1.0 - NATURAL_THRESHOLD)=" + mUpwardTrend * (1.0 - NATURAL_THRESHOLD));
+//                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: current.getLow()=" + current.getLow() + " < mUpwardTrend * (1.0 - NATURAL_THRESHOLD)=" + mUpwardTrend * (1.0 - NATURAL_THRESHOLD));
                         mPrevHigh = mUpwardTrend;
-                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: mPrevHigh = mUpwardTrend=" + mPrevHigh);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: mPrevHigh = mUpwardTrend=" + mPrevHigh);
                         mMarketKeyType = StockData.MARKET_KEY_NATURAL_REACTION;
-                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: mMarketKeyType = StockData.MARKET_KEY_NATURAL_REACTION");
+//                        Log.d(TAG, "case StockData.MARKET_KEY_UPWARD_TREND: mMarketKeyType = StockData.MARKET_KEY_NATURAL_REACTION");
                         setNaturalReaction(current);
                     }
                     break;
 
                 case StockData.MARKET_KEY_DOWNWARD_TREND:
-                    Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: mDownwardTrend=" + mDownwardTrend);
+//                    Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: mDownwardTrend=" + mDownwardTrend);
                     if (current.getLow() < mDownwardTrend) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: current.getLow()=" + current.getLow() + " < mDownwardTrend=" + mDownwardTrend);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: current.getLow()=" + current.getLow() + " < mDownwardTrend=" + mDownwardTrend);
                         setDownwardTrend(current);
                     } else if (current.getHigh() > mDownwardTrend * (1.0 + NATURAL_THRESHOLD)) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: current.getHigh()=" + current.getHigh() + " > mDownwardTrend * (1.0 + NATURAL_THRESHOLD)=" + mDownwardTrend * (1.0 + NATURAL_THRESHOLD));
+//                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: current.getHigh()=" + current.getHigh() + " > mDownwardTrend * (1.0 + NATURAL_THRESHOLD)=" + mDownwardTrend * (1.0 + NATURAL_THRESHOLD));
                         mPrevLow = mDownwardTrend;
-                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: mPrevLow = mDownwardTrend=" + mPrevLow);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: mPrevLow = mDownwardTrend=" + mPrevLow);
                         mMarketKeyType = StockData.MARKET_KEY_NATURAL_RALLY;
-                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: mMarketKeyType = StockData.MARKET_KEY_NATURAL_RALLY");
+//                        Log.d(TAG, "case StockData.MARKET_KEY_DOWNWARD_TREND: mMarketKeyType = StockData.MARKET_KEY_NATURAL_RALLY");
                         setNaturalRally(current);
                     }
                     break;
 
                 case StockData.MARKET_KEY_NATURAL_REACTION:
-                    Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mNaturalReaction=" + mNaturalReaction);
+//                    Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mNaturalReaction=" + mNaturalReaction);
                     if (current.getLow() < mNaturalReaction) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.getLow()=" + current.getLow() + " < mNaturalReaction=" + mNaturalReaction);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.getLow()=" + current.getLow() + " < mNaturalReaction=" + mNaturalReaction);
                         setNaturalReaction(current);
                         if (current.getLow() < mPrevLow * (1.0 - NATURAL_THRESHOLD / 2.0)) {
-                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.getLow()=" + current.getLow() + "  < mPrevLow * (1.0 - NATURAL_THRESHOLD / 2.0)=" +  mPrevLow * (1.0 - NATURAL_THRESHOLD / 2.0));
+//                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.getLow()=" + current.getLow() + "  < mPrevLow * (1.0 - NATURAL_THRESHOLD / 2.0)=" +  mPrevLow * (1.0 - NATURAL_THRESHOLD / 2.0));
                             mMarketKeyType = StockData.MARKET_KEY_DOWNWARD_TREND;
-                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mMarketKeyType = StockData.MARKET_KEY_DOWNWARD_TREND");
+//                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mMarketKeyType = StockData.MARKET_KEY_DOWNWARD_TREND");
                             setDownwardTrend(current);
                             current.setNaturalReaction(0);
-                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.setNaturalReaction(0)");
+//                            Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.setNaturalReaction(0)");
                         }
                     } else if (current.getHigh() > mNaturalReaction * (1.0 + NATURAL_THRESHOLD)) {
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.getHigh()=" + current.getHigh() + " > mNaturalReaction * (1.0 + NATURAL_THRESHOLD)=" + mNaturalReaction * (1.0 + NATURAL_THRESHOLD));
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: current.getHigh()=" + current.getHigh() + " > mNaturalReaction * (1.0 + NATURAL_THRESHOLD)=" + mNaturalReaction * (1.0 + NATURAL_THRESHOLD));
                         mPrevLow = mNaturalReaction;
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mPrevLow = mNaturalReaction=" + mPrevLow);
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mPrevLow = mNaturalReaction=" + mPrevLow);
                         mMarketKeyType = StockData.MARKET_KEY_NATURAL_RALLY;
-                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mMarketKeyType = StockData.MARKET_KEY_NATURAL_RALLY");
+//                        Log.d(TAG, "case StockData.MARKET_KEY_NATURAL_REACTION: mMarketKeyType = StockData.MARKET_KEY_NATURAL_RALLY");
                         setNaturalRally(current);
                     }
                     break;
@@ -149,25 +149,25 @@ public class MarketKeyAnalyzer {
                     break;
 
                 default:
-                    Log.d(TAG, "default:");
+//                    Log.d(TAG, "default:");
                     if (current.getHigh() > prev.getHigh()) {
-                        Log.d(TAG, "current.getHigh() > prev.getHigh()");
+//                        Log.d(TAG, "current.getHigh() > prev.getHigh()");
                         mMarketKeyType = StockData.MARKET_KEY_UPWARD_TREND;
-                        Log.d(TAG, "mMarketKeyType = StockData.MARKET_KEY_UPWARD_TREND");
+//                        Log.d(TAG, "mMarketKeyType = StockData.MARKET_KEY_UPWARD_TREND");
                         mUpwardTrend = current.getHigh();
                         mDownwardTrend = current.getHigh();
                         mPrevHigh = current.getHigh();
                         mPrevLow = current.getHigh();
-                        Log.d(TAG, "mUpwardTrend = mDownwardTrend = mPrevHigh = mPrevLow = " + current.getHigh());
+//                        Log.d(TAG, "mUpwardTrend = mDownwardTrend = mPrevHigh = mPrevLow = " + current.getHigh());
                     } else if (current.getLow() < prev.getLow()) {
-                        Log.d(TAG, "current.getLow() < prev.getLow()");
+//                        Log.d(TAG, "current.getLow() < prev.getLow()");
                         mMarketKeyType = StockData.MARKET_KEY_DOWNWARD_TREND;
-                        Log.d(TAG, "mMarketKeyType = StockData.MARKET_KEY_DOWNWARD_TREND");
+//                        Log.d(TAG, "mMarketKeyType = StockData.MARKET_KEY_DOWNWARD_TREND");
                         mUpwardTrend = current.getLow();
                         mDownwardTrend = current.getLow();
                         mPrevHigh = current.getLow();
                         mPrevLow = current.getLow();
-                        Log.d(TAG, "mUpwardTrend = mDownwardTrend = mPrevHigh = mPrevLow = " + current.getLow());
+//                        Log.d(TAG, "mUpwardTrend = mDownwardTrend = mPrevHigh = mPrevLow = " + current.getLow());
                     }
                     break;
             }
@@ -181,7 +181,7 @@ public class MarketKeyAnalyzer {
 
         mNaturalRally = stockData.getHigh();
         stockData.setNaturalRally(mNaturalRally);
-        Log.d(TAG, "setNaturalRally mNaturalRally=" + mNaturalRally);
+//        Log.d(TAG, "setNaturalRally mNaturalRally=" + mNaturalRally);
     }
 
 
@@ -192,7 +192,7 @@ public class MarketKeyAnalyzer {
 
         mUpwardTrend = stockData.getHigh();
         stockData.setUpwardTrend(mUpwardTrend);
-        Log.d(TAG, "setUpwardTrend mUpwardTrend=" + mUpwardTrend);
+//        Log.d(TAG, "setUpwardTrend mUpwardTrend=" + mUpwardTrend);
     }
 
     void setDownwardTrend(StockData stockData) {
@@ -202,7 +202,7 @@ public class MarketKeyAnalyzer {
 
         mDownwardTrend = stockData.getLow();
         stockData.setDownwardTrend(mDownwardTrend);
-        Log.d(TAG, "setDownwardTrend mDownwardTrend=" + mDownwardTrend);
+//        Log.d(TAG, "setDownwardTrend mDownwardTrend=" + mDownwardTrend);
     }
 
 
@@ -213,7 +213,7 @@ public class MarketKeyAnalyzer {
 
         mNaturalReaction = stockData.getLow();
         stockData.setNaturalReaction(mNaturalReaction);
-        Log.d(TAG, "setNaturalReaction mNaturalReaction=" + mNaturalReaction);
+//        Log.d(TAG, "setNaturalReaction mNaturalReaction=" + mNaturalReaction);
     }
 
 }
