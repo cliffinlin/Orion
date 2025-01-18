8af1e6ec2efd6475ac33856add3e83a349c97aeb
linjingming <13520500645@139.com>
2025-01-18 08:39:45 +0800

    update

--- app/src/main/java/com/android/orion/ai/ml/learning/HousePrices.java
+++ app/src/main/java/com/android/orion/ai/ml/learning/HousePrices.java
@@ -7,13 +7,13 @@ public class HousePrices {
 
 	double[] xArray = new double[]{32,53,61,47,59,55,52,39,48,52,45,54,44,58,56,48,44,60};
 	double[] yArray = new double[]{31,68,62,71,87,78,79,59,75,71,55,82,62,75,81,60,82,97};
+	Trainer trainer = new Trainer(xArray, yArray);
 
 	public HousePrices() {
-		Trainer trainer = new Trainer(xArray, yArray);
 	}
 
 	public void test() {
-		Trainer trainer = new Trainer(xArray, yArray);
+		trainer = new Trainer(xArray, yArray);
 		for (int i = 100; i < 10000; i+=100) {
 			trainer.train(i);
 			Log.d(TAG, "training " + i + " times: " + trainer.toString());
