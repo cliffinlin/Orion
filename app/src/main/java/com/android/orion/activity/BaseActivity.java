e117e7cd1087666e2606ce30302cfa3ba65651be
linjingming <13520500645@139.com>
2025-03-05 00:11:16 +0800

    update

--- app/src/main/java/com/android/orion/activity/BaseActivity.java
+++ app/src/main/java/com/android/orion/activity/BaseActivity.java
@@ -7,6 +7,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Message;
 import android.util.ArrayMap;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -15,6 +19,7 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 
+import com.android.orion.application.MainApplication;
 import com.android.orion.database.Stock;
 import com.android.orion.interfaces.AnalyzeListener;
 import com.android.orion.interfaces.DownloadListener;
@@ -27,6 +32,16 @@ import com.android.orion.utility.Logger;
 import java.util.ArrayList;
 
 public class BaseActivity extends Activity implements AnalyzeListener, DownloadListener {
+
+	public static final int MESSAGE_ON_CREATE = 1000;
+	public static final int MESSAGE_ON_RESUME = 1001;
+	public static final int MESSAGE_ON_PAUSE = 1002;
+	public static final int MESSAGE_ON_DESTROY = 1003;
+	public static final int MESSAGE_ON_STOP = 1004;
+	public static final int MESSAGE_ON_START = 1005;
+	public static final int MESSAGE_ON_RESTART = 1006;
+	public static final int MESSAGE_ON_NEW_INTENT = 1007;
+
 
 	private static final int REQUEST_EXTERNAL_STORAGE = 1;
 	private static final String[] PERMISSIONS_STORAGE = {"android.permission.READ_EXTERNAL_STORAGE",
@@ -45,44 +60,45 @@ public class BaseActivity extends Activity implements AnalyzeListener, DownloadL
 	DatabaseManager mDatabaseManager = DatabaseManager.getInstance();
 	IStockDataProvider mStockDataProvider = StockDataProvider.getInstance();
 
+	HandlerThread mHandlerThread;
+	ServiceHandler mServiceHandler;
+	String TAG = getClass().getSimpleName();
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		mContext = this;
-		mIntent = getIntent();
-		if (mIntent != null) {
-			mAction = mIntent.getAction();
-			mBundle = mIntent.getExtras();
-		}
-
-		mStockDataProvider.registerAnalyzeListener(this);
-		mStockDataProvider.registerDownloadListener(this);
-	}
-
-	@Override
-	protected void onPause() {
-		super.onPause();
-		mResumed = false;
+		init();
+		mServiceHandler.sendEmptyMessage(MESSAGE_ON_CREATE);
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		checkPermission();
-		mResumed = true;
+		mServiceHandler.sendEmptyMessage(MESSAGE_ON_RESUME);
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mServiceHandler.sendEmptyMessage(MESSAGE_ON_PAUSE);
 	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-		mStockDataProvider.unRegisterAnalyzeListener(this);
-		mStockDataProvider.unRegisterDownloadListener(this);
+		mServiceHandler.sendEmptyMessage(MESSAGE_ON_DESTROY);
+	}
+
+	@Override
+	protected void onNewIntent(Intent intent) {
+		super.onNewIntent(intent);
+		setIntent(intent);
+		mServiceHandler.sendEmptyMessage(MESSAGE_ON_NEW_INTENT);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		getActionBar().setDisplayHomeAsUpEnabled(true);
-
 		return super.onCreateOptionsMenu(menu);
 	}
 
@@ -92,22 +108,8 @@ public class BaseActivity extends Activity implements AnalyzeListener, DownloadL
 			case android.R.id.home:
 				finish();
 				return true;
-
 			default:
 				return super.onMenuItemSelected(featureId, item);
-		}
-	}
-
-	@Override
-	protected void onNewIntent(Intent intent) {
-		super.onNewIntent(intent);
-
-		setIntent(intent);
-
-		mIntent = getIntent();
-		if (mIntent != null) {
-			mAction = mIntent.getAction();
-			mBundle = mIntent.getExtras();
 		}
 	}
 
@@ -161,6 +163,80 @@ public class BaseActivity extends Activity implements AnalyzeListener, DownloadL
 	public void onDownloadComplete(String stockCode) {
 		if (mStock.getCode().equals(stockCode)) {
 			restartLoader();
+		}
+	}
+
+	void init() {
+		mHandlerThread = new HandlerThread(TAG);
+		mHandlerThread.start();
+		mServiceHandler = new ServiceHandler(mHandlerThread.getLooper());
+	}
+
+	class ServiceHandler extends Handler {
+
+		public ServiceHandler(Looper looper) {
+			super(looper);
+		}
+
+		public void handleMessage(Message msg) {
+			if (msg == null) {
+				return;
+			}
+
+			switch (msg.what) {
+				case MESSAGE_ON_CREATE:
+					onCreateHandler();
+					break;
+				case MESSAGE_ON_RESUME:
+					onResumeHandler();
+					break;
+				case MESSAGE_ON_PAUSE:
+					onPauseHandler();
+					break;
+				case MESSAGE_ON_DESTROY:
+					onDestroyHandler();
+					break;
+				case MESSAGE_ON_STOP:
+					break;
+				case MESSAGE_ON_START:
+					break;
+				case MESSAGE_ON_RESTART:
+					break;
+				case MESSAGE_ON_NEW_INTENT:
+					onNewIntentHandler();
+					break;
+			}
+		}
+	}
+
+	void onCreateHandler() {
+		mContext = this;
+		onNewIntentHandler();
+		mStockDataProvider.registerAnalyzeListener(this);
+		mStockDataProvider.registerDownloadListener(this);
+	}
+
+	void onResumeHandler() {
+		checkPermission();
+		mResumed = true;
+	}
+
+	void onPauseHandler() {
+		mResumed = false;
+	}
+
+	void onDestroyHandler() {
+		mStockDataProvider.unRegisterAnalyzeListener(this);
+		mStockDataProvider.unRegisterDownloadListener(this);
+
+		mHandlerThread.quitSafely();
+	}
+
+	void onNewIntentHandler() {
+		mIntent = getIntent();
+		if (mIntent != null) {
+			mAction = mIntent.getAction();
+			mBundle = mIntent.getExtras();
 		}
 	}
 }
