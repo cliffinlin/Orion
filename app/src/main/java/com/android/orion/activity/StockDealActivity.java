2096e9989021aa81ff4eeb328ee1a5669a2c2f9e
linjingming <13520500645@139.com>
2025-11-23 00:08:45 +0800

    update

--- app/src/main/java/com/android/orion/activity/StockDealActivity.java
+++ app/src/main/java/com/android/orion/activity/StockDealActivity.java
@@ -382,7 +382,7 @@ public class StockDealActivity extends DatabaseActivity implements
 				String dealDate = mEditTextDealDate.getText().toString();
 				if (TextUtils.isEmpty(dealDate)) {
 					Toast.makeText(mContext, R.string.stock_deal_date_empty, Toast.LENGTH_LONG).show();
-					return;
+//					return;
 				}
 				mDeal.setDate(dealDate);
 
