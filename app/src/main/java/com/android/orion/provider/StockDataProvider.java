c1e68fd87427d52c1524663772f213f171b939cb
linjingming <13520500645@139.com>
2026-03-26 11:44:23 +0800

    update

--- app/src/main/java/com/android/orion/provider/StockDataProvider.java
+++ app/src/main/java/com/android/orion/provider/StockDataProvider.java
@@ -842,6 +842,14 @@ public class StockDataProvider implements StockListener, IStockDataProvider {
 						handleAnalyzeMessage(stock);
 					}
 				}
+
+				if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.M) {
+					if (getLooper().getQueue().isIdle()) {
+						if (Market.isTradingHours()) {
+							download();
+						}
+					}
+				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			} finally {
